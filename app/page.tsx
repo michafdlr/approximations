55d384281075007914f8526d53e92569a8443332
Michael Fiedler <michael.fiedler1604@gmail.com>
2024-09-22 18:40:11 +0200

    Started Card-Component for different procedures

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Link from 'next/link';
+import Image from 'next/image'
 
 export default function Home() {
   return (
       <main className="flex min-h-screen flex-col items-center justify-normal text-justify p-2">
-        <h1 className="font-bold text-xl">Näherung von Quadratwurzeln</h1>
+        <h1 className="font-bold text-2xl">Näherung von Quadratwurzeln</h1>
         <p>
           Auf diesen Seiten kannst Du zwei verschiedene Verfahren zur näherungsweisen Berechnung von Quadratwurzeln kennenlernen, das <Link
             href="/intervallhalbierung"
@@ -21,6 +22,24 @@ export default function Home() {
             </strong>
           </Link>.
         </p>
+        <Card heading='Intervallhalbierungsverfahren' imgSrc='ival.png'/>
       </main>
   );
+}
+
+function Card({heading, imgSrc}: {heading: string, imgSrc: string}) {
+  return (
+    <div className='flex flex-col w-1/4 mb-2 mt-8 p-2 border-2 rounded-md transition-all duration-200 hover:translate-x-1 hover:translate-y-1 hover:text-blue-400 hover:border-blue-400 cursor-pointer'>
+      <h1 className='text-xl mb-2 font-extrabold underline'>{heading}</h1>
+      <p className='text-lg mb-3'>
+        Ein einfaches rekursives Verfahren zur näherungsweisen Bestimmung des Werts des Wurzelterms durch sukzessives Halbieren eines Intervalls, in dem der Wert liegt.
+      </p>
+      <Image
+        src={`/${imgSrc}`}
+        width={500}
+        height={500}
+        alt="Bild der Zahlengeraden"
+      />
+    </div>
+  )
 }
