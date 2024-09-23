8b7b07a94c8ac2fbcec29b3ddb12afd99545180c
Michael Fiedler <michael.fiedler1604@gmail.com>
2024-09-23 06:24:10 +0200

    new elements in Card Component

--- app/page.tsx
+++ app/page.tsx
@@ -22,24 +22,32 @@ export default function Home() {
             </strong>
           </Link>.
         </p>
-        <Card heading='Intervallhalbierungsverfahren' imgSrc='ival.png'/>
+        <div className='flex flex-row gap-2 justify-center'>
+          <Card link='intervallhalbierung' heading='Intervallhalbierungsverfahren' imgSrc='ival.png' pText='Ein einfaches rekursives Verfahren zur näherungsweisen Bestimmung des Werts des Wurzelterms durch sukzessives Halbieren eines Intervalls, in dem der Wert liegt.' altText='Bild der Zahlengeraden' width={500} height={500}/>
+          <Card link='heron' heading='Heronverfahren' pText='Ein rekursives Verfahren zur näherungsweisen Bestimmung des Werts des Wurzelterms durch Mittelwertbildung eines Rechtecks mit Flächeninhalt entsprechend des Radikanden.' imgSrc='heron.png' altText='Bild eines Rechtecks' width={500} height={500}/>
+        </div>
       </main>
   );
 }
 
-function Card({heading, imgSrc}: {heading: string, imgSrc: string}) {
+function Card({link, heading, imgSrc, pText, altText, width, height}: {link: string, heading: string, imgSrc: string, pText: string, altText: string, width: number, height: number}) {
   return (
-    <div className='flex flex-col w-1/4 mb-2 mt-8 p-2 border-2 rounded-md transition-all duration-200 hover:translate-x-1 hover:translate-y-1 hover:text-blue-400 hover:border-blue-400 cursor-pointer'>
-      <h1 className='text-xl mb-2 font-extrabold underline'>{heading}</h1>
-      <p className='text-lg mb-3'>
-        Ein einfaches rekursives Verfahren zur näherungsweisen Bestimmung des Werts des Wurzelterms durch sukzessives Halbieren eines Intervalls, in dem der Wert liegt.
-      </p>
-      <Image
-        src={`/${imgSrc}`}
-        width={500}
-        height={500}
-        alt="Bild der Zahlengeraden"
-      />
-    </div>
+    <Link
+      href={`/${link}`}
+      key={link}
+    >
+      <div className='flex flex-col mb-2 mt-8 p-2 border-2 rounded-md transition-all duration-200 hover:translate-x-1 hover:translate-y-1  hover:border-blue-400 cursor-pointer'>
+        <h1 className='text-xl mb-2 font-extrabold underline'>{heading}</h1>
+        <p className='text-lg mb-3'>
+          {pText}
+        </p>
+        <Image
+          src={`/${imgSrc}`}
+          width={width}
+          height={height}
+          alt={altText}
+        />
+      </div>
+    </Link>
   )
 }
