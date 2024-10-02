e0a7b2f09f3cc975111dc26f7c6643954b29f00d
Michael Fiedler <michael.fiedler1604@gmail.com>
2024-10-02 16:38:45 +0200

    minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -22,9 +22,9 @@ export default function Home() {
             </strong>
           </Link>.
         </p>
-        <div className='flex flex-row gap-2 justify-center'>
-          <Card link='intervallhalbierung' heading='Intervallhalbierungsverfahren' imgSrc='ival.png' pText='Ein einfaches rekursives Verfahren zur näherungsweisen Bestimmung des Werts des Wurzelterms durch sukzessives Halbieren eines Intervalls, in dem der Wert liegt.' altText='Bild der Zahlengeraden' width={500} height={500}/>
-          <Card link='heron' heading='Heronverfahren' pText='Ein rekursives Verfahren zur näherungsweisen Bestimmung des Werts des Wurzelterms durch Mittelwertbildung eines Rechtecks mit Flächeninhalt entsprechend des Radikanden.' imgSrc='heron.png' altText='Bild eines Rechtecks' width={500} height={500}/>
+        <div className='flex flex-row flex-wrap gap-2 justify-center w-full'>
+          <Card link='intervallhalbierung' heading='Intervallhalbierungsverfahren' imgSrc='ival.png' pText='Ein einfaches rekursives Verfahren zur näherungsweisen Bestimmung des Werts des Wurzelterms durch sukzessives Halbieren eines Intervalls, in dem der Wert liegt.' altText='Bild der Zahlengeraden' width={400} height={400}/>
+          <Card link='heron' heading='Heronverfahren' pText='Ein rekursives Verfahren zur näherungsweisen Bestimmung des Werts des Wurzelterms durch Mittelwertbildung eines Rechtecks mit Flächeninhalt entsprechend des Radikanden.' imgSrc='heron.png' altText='Bild eines Rechtecks' width={400} height={400}/>
         </div>
       </main>
   );
@@ -32,22 +32,26 @@ export default function Home() {
 
 function Card({link, heading, imgSrc, pText, altText, width, height}: {link: string, heading: string, imgSrc: string, pText: string, altText: string, width: number, height: number}) {
   return (
-    <Link
-      href={`/${link}`}
-      key={link}
-    >
-      <div className='flex flex-col mb-2 mt-8 p-2 border-2 rounded-md transition-all duration-200 hover:translate-x-1 hover:translate-y-1  hover:border-blue-400 cursor-pointer'>
-        <h1 className='text-xl mb-2 font-extrabold underline'>{heading}</h1>
-        <p className='text-lg mb-3'>
-          {pText}
-        </p>
-        <Image
-          src={`/${imgSrc}`}
-          width={width}
-          height={height}
-          alt={altText}
-        />
-      </div>
-    </Link>
+    <div className='max-w-md'>
+      <Link
+        href={`/${link}`}
+        key={link}
+      >
+        <div className='flex flex-col h-full items-center mb-2 mt-8 p-2 border-2 rounded-md transition-all duration-200 hover:translate-x-1 hover:translate-y-1  hover:border-blue-400 cursor-pointer'>
+          <h1 className='text-xl mb-2 font-extrabold underline'>{heading}</h1>
+          <p className='text-lg mb-3'>
+            {pText}
+          </p>
+          <Image
+            unoptimized
+            src={`/${imgSrc}`}
+            width={width}
+            height={height}
+            alt={altText}
+            style={{ width: '100%', height: 'auto' }}
+          />
+        </div>
+      </Link>
+    </div>
   )
 }
